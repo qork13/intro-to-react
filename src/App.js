7fd412386a0b3b8d0e86428497390a83737e35c3
qork13 <57408249+qork13@users.noreply.github.com>
2021-04-26 20:31:33 -0500

    Making movies carousel

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
 import './App.css';
-import React from "react";
-import Button from "./Button.js";
+import React, {useState} from "react";
+import {movies} from "./movies";
+
 
 
 function App() {
-  const [counter, setCounter] = React.useState(0);
+  const [currentMovie, setCurrentMovie] = useState(null);
 
-  const increment = () => {
-    setCounter(counter + 1);
-  }
-
-  const decrement = () => {
-    setCounter(counter - 1);
-  }
+  
   return (
     <div>
-      <p>Counter</p>
+      <h1>Movie Carousel</h1>
 
+      {
+        movies.map((movie) => {
+          return (
+            <button key={movie.title} onClick={() => setCurrentMovie(movie)}>
+                <p>{movie.title}</p>
+                <img src={movie.posterUrl} height={250}/>
+            </button>
+          )
+        })
+      }
       
-      <p>{counter}</p>
-
-      <Button onClickFunction = {increment} buttonText = {"+"}/>
-      <Button onClickFunction = {decrement} buttonText = {"-"}/>
-
+      {
+        currentMovie ? 
+        <>
+        <p>{currentMovie.title}</p>
+        <p>{currentMovie.description}</p>
+        <p>{currentMovie.rating}</p> 
+        <p>{currentMovie.director}</p> 
+        <p>{currentMovie.categories.join(", ")}</p> 
+        </>
+        : <p>Please select a movie</p>
+      }
+      
     </div>
   );
 }
